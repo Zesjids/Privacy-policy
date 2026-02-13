78146d6426a59d67aa30de2fa52ac08b4fd46e45
Ajida Moses <ajidamoses@gmail.com>
2026-02-13 07:23:06 +0100

    switched from svelte to html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,svelte}"],
+module.exports = {
+  content: ["./index.html"],
   theme: {
     extend: {},
   },
